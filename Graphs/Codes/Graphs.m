e5b7a8c0cb3085fe9932515f55ff1e5cd7455c53
NavodyaWijekoon <nyashikalaxp@gmail.com>
2025-06-03 00:05:38 +0530

    Add files via upload

---
+++ Graphs/Codes/Graphs.m
@@ -0,0 +1,7 @@
+s = [1 2]; %start nodes
+t = [2 3]; %End node
+
+G = graph(s,t); % create a graph object
+plot(G); 5 % plot the graph
+title('Graph'); % add a title
+
---
+++ Graphs/README.md
@@ -0,0 +1,5 @@
+1)Graphs
+2)Directed_graph
+3)Undirected_graph
+4)Unweighted_Graph
+5)Weighted_Undirected_graph
